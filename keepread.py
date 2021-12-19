8ca7607915ad0c1f5a738d3c19802a3c981577c1
Exyloss <antoninpons@protonmail.com>
2021-12-19 18:08:53 +0100

    commit

--- keepread.py
+++ keepread.py
@@ -4,24 +4,6 @@ import sys
 import getpass
 import subprocess
 
-#Variable à éditer
-path=""
-print(path)
-
-if path == "":
-    print("Veuillez editer la variable path avec le chemin vers votre base de données dans le fichier keepread.py.")
-    quit()
-
-
-while True:
-    r=getpass.getpass("mot de passe : ")
-    try:
-        kp = pkp(path, password=r)
-        break
-    except:
-        print("Mot de passe erroné.")
-        continue
-
 def copy(text):
     text = str(text)
     p = subprocess.Popen(['xclip', '-selection', "clip"],
@@ -34,6 +16,7 @@ def new_entry(arg):
         if arg == "q":
             quit()
         else:
+            print_entries()
             r = input("Erreur, l'id. n'existe pas.\nnom de l'id. : ")
             return new_entry(r)
     else:
@@ -71,6 +54,40 @@ d : supprimer l'entrée \n\
 h : afficher cette aide \n\
 q : quitter.")
 
+def print_entry(entry):
+    try:
+        totp = subprocess.check_output(
+            "totp.sh "+entry.get_custom_property("otp"), shell=True
+        ).decode('utf-8').replace("\n", "")
+    except:
+        totp = None
+    if totp == None:
+        print("\033[4m"+entry.title+"\033[0m")
+        print("Nom d'utilisateur : "+entry.username+"\nMot de passe : "+"*"*len(entry.password))
+    else:
+        print("\033[4m"+entry.title+"\033[0m")
+        print("Nom d'utilisateur : "+entry.username+"\nMot de passe : "+"*"*len(entry.password)+"\nTOTP : "+totp)
+
+
+#Variable à éditer
+path=""
+print(path)
+
+if path == "":
+    print("Veuillez editer la variable path avec le chemin vers votre base de données dans le fichier keepread.py.")
+    quit()
+
+
+while True:
+    r=getpass.getpass("mot de passe : ")
+    try:
+        kp = pkp(path, password=r)
+        break
+    except:
+        print("Mot de passe erroné.")
+        continue
+
+
 if len(sys.argv) == 1 or sys.argv[1] == "-l":
     print_entries()
     r = input("Nom de l'id. ( q = quitter ) : ")
@@ -84,35 +101,27 @@ elif sys.argv[1] == "-d" and len(sys.argv) == 3:
 else:
     entry = new_entry(sys.argv[1])
 
+print_entry(entry)
 while True:
-    try:
-        totp = subprocess.check_output(
-            "totp.sh "+entry.get_custom_property("otp"), shell=True
-        ).decode('utf-8').replace("\n", "")
-    except:
-        totp = None
-    if totp == None:
-        print("\033[4m"+entry.title+"\033[0m")
-        print("Nom d'utilisateur : "+entry.username+"\nMot de passe : "+"*"*len(entry.password))
-    else:
-        print("\033[4m"+entry.title+"\033[0m")
-        print("Nom d'utilisateur : "+entry.username+"\nMot de passe : "+"*"*len(entry.password)+"\n TOTP : "+totp)
     r = input(":")
     if r == "q":
         break
     elif r == "m":
         copy(entry.password)
+        print("Mot de passe copié.")
     elif r == "n":
         copy(entry.username)
+        print("Nom d'utilisateur copié.")
     elif r == "r":
-        print_entries()
         r = input("Nom de l'id. : ")
         entry = new_entry(r)
+        print_entry(entry)
     elif r == "t":
         totp = subprocess.check_output(
             "totp.sh "+entry.get_custom_property("otp"), shell=True
         ).decode('utf-8').replace("\n", "")
         copy(totp)
+        print("TOTP copié.")
     elif r == "l":
         print_entries()
     elif r == "d":
