12fa0af1f212510347b550ccc38d90ffeb590478
Exyloss <antoninpons@protonmail.com>
2021-12-22 16:51:16 +0100

    commit

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-[ "$1" = "" ] && exit 1
+[ "$1" = "" ] && echo "Erreur : veuillez renseigner le chemin vers votre base de données en argument." && exit 1
 ans=$(echo "$1" | sed "s|~|/home/$USER|")
 cp keepread.py ~/.local/bin/keepread
 cp keepread-dmenu.py ~/.local/bin/keepread-dmenu
