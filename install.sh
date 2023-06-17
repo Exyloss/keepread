4506e3a69257b0221ead046a1457790d4b593280
Antonin PONS <antoninpons@protonmail.com>
2023-06-17 19:23:28 +0200

    commit

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 [ "$1" = "" ] && echo "Erreur : veuillez renseigner le chemin vers votre base de données en argument." && exit 1
+mkdir "$XDG_CONFIG_HOME"/keepread
 cp config.ini "$XDG_CONFIG_HOME"/keepread/config.ini
 sed -i "s|^path=$|path=$1|" "$XDG_CONFIG_HOME"/keepread/config.ini && echo "La configuration a bien été appliquée."
-mkdir "$XDG_CONFIG_HOME"/keepread 2>/dev/null
 cp kr.py ~/.local/bin/kr
 cp dkr.py ~/.local/bin/dkr
 cp totp.sh ~/.local/bin
